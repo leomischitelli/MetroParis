b5060b68360bb9718386aadbaf620e1b2a829618
Leo <Leo@PC-Crumb.home-life.hub>
2022-05-09 12:22:19 +0200

    Versione effettivamente senza commenti

--- src/main/java/it/polito/tdp/metroparis/model/Model.java
+++ src/main/java/it/polito/tdp/metroparis/model/Model.java
@@ -43,76 +43,7 @@ public class Model {
 		return null;
 	}
 	
-/*	public void creaGrafo() {
-		this.grafo = new SimpleDirectedGraph<Fermata, DefaultEdge>(DefaultEdge.class);
-		MetroDAO dao = new MetroDAO();
-		List<Fermata> fermate = dao.getAllFermate();
-		Map<Integer, Fermata> fermateIdMap = new HashMap<Integer, Fermata>();
-		for(Fermata f : fermate) {
-			fermateIdMap.put(f.getIdFermata(), f);
-		}
-		Graphs.addAllVertices(this.grafo, fermate);
-		
-		//metodo 1: itero su ogni coppia di vertici
-//		for(Fermata partenza : fermate) {
-//			for(Fermata arrivo : fermate) {
-//				if(dao.isFermataConnessaFermata(partenza, arrivo)) {
-//					this.grafo.addEdge(partenza, arrivo);
-//				}
-//			}
-//		}
-		
-		//metodo 2: dato ciascun vertice, trovo i vertici ad esso adiacenti
-		//posso iterare su fermate oppure su this.grafo.vertexSet()
-//		for(Fermata partenza : fermate) {
-//			List<Integer> idConnesse = dao.getIdFermateConnesse(partenza);
-//			for(Integer id : idConnesse) {
-//		//		Fermata arrivo = (fermata che possiede questo "id");
-//				Fermata arrivo = null;
-//				for(Fermata f : fermate) {
-//					if(f.getIdFermata() == id) {
-//						arrivo = f;
-//						break;
-//					}
-//				}
-//				this.grafo.addEdge(partenza, arrivo);
-//			}
-//		}
-		
-		
-//		metodo 2b: il DAO restituisce un elenco di oggetti fermata
-		
-		
-		for(Fermata partenza: fermate) {
-			List<Fermata> arrivi = dao.getFermateConnesse(partenza);
-			for(Fermata arrivo : arrivi) {
-				this.grafo.addEdge(partenza, arrivo);
-			}
-		}
-		
-		
-		//metodo 2c: il DAO restituisce id numerici che converto in oggetti tramte una map<Integer, Fermata> (identity map)
-		for(Fermata partenza : fermate) {
-			List<Integer> idConnesse = dao.getIdFermateConnesse(partenza);
-			for(int id : idConnesse) {
-				Fermata arrivo = fermateIdMap.get(id);
-				this.grafo.addEdge(partenza, arrivo);
-			}
-		} 
-		
-		//metodo 3: faccio una sola query che restituisce le coppie di fermate da collegare (preferisco 3c con identity map
-		List<CoppiaId> fermateDaCollegare = dao.getAllFermateConnesse();
-		for(CoppiaId coppia : fermateDaCollegare) {
-			this.grafo.addEdge(fermateIdMap.get(coppia.getIdPartenza()),
-					fermateIdMap.get(coppia.getIdArrivo()));
-		}
-		
-		System.out.println(this.grafo);
-		System.out.println("Vertici: " + this.grafo.vertexSet().size());
-		System.out.println("Archi: " + this.grafo.edgeSet().size());
 
-		visitaGrafo(fermate.get(0));
-	}  */
 	
 	public void creaGrafo() {
 		this.grafo = new SimpleDirectedGraph<Fermata, DefaultEdge>(DefaultEdge.class);
@@ -126,8 +57,6 @@ public class Model {
 		
 
 	}
-	
-	
 	
 	
 	public Map<Fermata, Fermata> visitaGrafo(Fermata partenza) {
